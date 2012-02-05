e281cd7ac2a4e1d8914d3d9082faa10f7ed31720
Joseph Hallett <bogwonch@Snowwhite.local>
2012-02-05 15:24:31 +0000

    Added support for XMOS's hideously borked toolchain

--- bin/getopcode.rb
+++ bin/getopcode.rb
@@ -10,6 +10,7 @@ $as       = 'as'
 $as_flags = ''
 $objdump  = 'objdump'
 $machine  = 'x86'
+$xmos     = false
 
 opts = OptionParser.new do |opts|
   opts.banner = "Usage: getopcode.rb [options]"
@@ -20,7 +21,8 @@ opts = OptionParser.new do |opts|
   opts.on("-d", "--dump PROG", "Version of objdump to use") { |prog| $objdump = prog    }
   opts.on("-f", "--flags FLAGS", "Flags to pass to the assembler") { |flag| $as_flags = flag    }
   opts.on("-m", "--machine MACHINE", "Machine we are assembling on") { |mach| $machine = mach }
-  opts.on("-h", "--help",            "Show this message") do
+  opts.on("-x", "--xmos", "Use the XMOS alternate syntax for objdump") { $xmos = true }
+  opts.on("-h", "--help", "Show this message") do
     puts opts
     exit
   end
@@ -31,17 +33,37 @@ opts.parse!(ARGV)
 # Assemble an assembly instruction and get the bytecode generated
 def assemble(op)
   dump = ""
-  Tempfile.open('getopcode') do |f|
+  Tempfile.open(['getopcode','.o']) do |f|
     `echo '#{op}' | #{$as} #{$flags} -o #{f.path}`
     dump = `#{$objdump} -d #{f.path}`
   end
 
+  unless $xmos 
+    return extract_opcode(dump)
+  else
+    return xmos_extract_opcode(dump)
+  end
+end
+
+# Extract the opcode out of the dump from objdump
+def extract_opcode(dump)
   dump.each_line do |line|
     # If the line is a line of disassembled code...
     m = /\A\s+[0-9a-f]+:/i.match line
 
     # Extract the opcode field and remove any internal spaces
     return line.split(/\t/)[1].split.join unless m.nil?
+  end
+end
+
+# A Special case for the XMOS development kit
+def xmos_extract_opcode(dump)
+  dump.each_line do |line|
+    # If the line is a line of dissassembled code
+    m = /\A\s+0x[0-9a-f]+:/i.match line
+
+    # Extract the opcode field and remove any internal spaces
+    return line.split(/:/)[1].split.join unless m.nil?
   end
 end
 
@@ -54,6 +76,7 @@ def parse_input(line)
 end
 
 
+# Assemble the input
 ARGF.each_line do |line|
   line.chomp!
   prefixes, suffixes = parse_input line
